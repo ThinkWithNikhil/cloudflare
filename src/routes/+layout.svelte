524a2215c6cb38251487fad4ccdfd15f47f0e54b
Nikhil Tirumalasetty <tirumalasettynikhil@gmail.com>
2025-11-19 10:53:29 +0530

    feat: implement sidebar layout with account management dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Cloudflare</title>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+	import SettingsIcon from "@lucide/svelte/icons/settings";
+	import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar variant='inset' />
+	<Sidebar.Inset>
+		<header
+			class="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear"
+		>
+			<div class="flex items-center gap-2 px-4">
+				<Sidebar.Trigger class="-ml-1" />
+				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+				<Breadcrumb.Root>
+					<Breadcrumb.List>
+						<Breadcrumb.Item class="hidden md:block">
+							<Breadcrumb.Page>Home</Breadcrumb.Page>
+						</Breadcrumb.Item>
+					</Breadcrumb.List>
+				</Breadcrumb.Root>
+			</div>
+			<div class="ml-auto flex items-center gap-2">
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger >
+						<Button variant="outline" size="sm" class="mr-2">
+							<SettingsIcon />
+							Manage Account
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end" sideOffset={8} class="mr-2">
+						<DropdownMenu.Group>
+							<DropdownMenu.Item>Settings</DropdownMenu.Item>
+							<DropdownMenu.Item>Billing</DropdownMenu.Item>
+							<DropdownMenu.Item>Team</DropdownMenu.Item>
+							<DropdownMenu.Item>API Tokens</DropdownMenu.Item>
+							<DropdownMenu.Item>Blocked Content</DropdownMenu.Item>
+							<DropdownMenu.Item>Abuse Reports</DropdownMenu.Item>
+							<DropdownMenu.Item>Carbon Impact Report</DropdownMenu.Item>
+						</DropdownMenu.Group>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
+		</header>
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+			</div>
+			<div class="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min"></div>
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
